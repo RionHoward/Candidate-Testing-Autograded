c637483859c26e2223c1d9e9d2e338b4ff680d80
RionHoward <148999262+RionHoward@users.noreply.github.com>
2023-10-31 14:54:13 -0400

    New Save

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,17 +3,14 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 // new save on github
 // TODO 1.1a: Define candidateName // 
-let candidateName = input.question("What is your name?");
-console.log(candidateName + " I hope you are doing well today!");;
+let candidateName = ""
+
+
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = ("Who was the first American woman in space? ");
 let correctAnswer = ("Sally Ride");
-let candidateAnswer = input.question("Who was the first American woman in space? ")
-if (candidateAnswer === correctAnswer){
-  console.log("Great job that is correct!");
-  } else {
-    console.log("Sorry that is incorrect!");
-  }
+let candidateAnswer = ""
+
 
 //TODO: Variables for Part 2
 let questions;
@@ -21,22 +18,30 @@ let correctAnswers;
 let candidateAnswers;
 
 
-function askForName(){
+function askForName() 
+{
 
   // TODO 1.1b: Ask for candidate's name //
-  
+  let candidateName = input.question("What is your name?");
+  console.log(candidateName + " I hope you are doing well today!");
 }
 
-function askQuestion() {
+function askQuestion() 
+{
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+  candidateAnswer = input.question(question);
 
 }
 
-function gradeQuiz(candidateAnswers) {
+function gradeQuiz() 
+{
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+  if (candidateAnswer === correctAnswer){
+    console.log("Great job that is correct!");
+    } else {
+      console.log("Sorry that is incorrect!");
+    }
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
